4eceafb80d12fc07dadf52c0cd453bea003e42b7
mmuttoni <mmuttoni@student.ethz.ch>
2025-03-13 09:41:12 +0100

    Adjusted python importing and defined main module

---
+++ src/GridCalEngine/Devices/Dynamic/__main__.py
@@ -0,0 +1,10 @@
+"""
+Dynamic simulator entry point. To run the _main_ module of the Dynamic package:
+    cd src
+    python -m GridCalEngine.Devices.Dynamic
+"""
+
+from GridCalEngine.Devices.Dynamic import main
+
+if __name__ == '__main__':
+    main()
