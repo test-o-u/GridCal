71db2f18775bc0f3e687931d991d673464063eb3
mmuttoni <mmuttoni@student.ethz.ch>
2025-03-13 12:11:31 +0100

    First version of the main() function

--- src/GridCalEngine/Devices/Dynamic/main.py
+++ src/GridCalEngine/Devices/Dynamic/main.py
@@ -2,4 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 # SPDX-License-Identifier: MPL-2.0
-import Dynamic
+from GridCalEngine.Devices import Dynamic
+
+def test():
+    print(Dynamic.System.models)
+    print(Dynamic.System.components)
