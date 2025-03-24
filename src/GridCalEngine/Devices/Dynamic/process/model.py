f40ca184214a3c8ddcb7178140292439c6b663f3
mmuttoni <mmuttoni@student.ethz.ch>
2025-03-24 11:55:10 +0100

    Deleted useless modules

--- src/GridCalEngine/Devices/Dynamic/process/model.py
+++
@@ -1,6 +0,0 @@
-class Model: 
-    def __init__(self): 
-        self.f = ["x**2 + y + sin(x) + cos(y)", "x*y + exp(y)"]
-        self.g = ["exp(a) + log(b) + a*b - a**2", "a**3 - b**3"]
-        self.state = ["x", "y"]
-        self.algeb = ["a", "b"]
