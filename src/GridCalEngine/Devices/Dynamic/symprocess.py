590c1576ec2f2824ea7755bf7f0e61e0c9cc5876
marina <mroses@eroots.tech>
2025-03-30 15:40:26 +0200

    dynamic variables class

--- src/GridCalEngine/Devices/Dynamic/symprocess.py
+++ src/GridCalEngine/Devices/Dynamic/symprocess.py
@@ -49,19 +49,11 @@ class SymProcess:
         self.g_list = []
         self.f_matrix = ()
         self.g_matrix = ()
-        self.f_jacob_sym = sp.Matrix([])
-        self.g_jacob_sym = sp.Matrix([])
-        self.symb_vars_dict = {}
         self.lambda_equations = {}
 
         # Jacobians
         self.jacob_states = []
         self.jacob_algebs = []
-        self.all_variables = []
-        self.sym_variables = []
-        self.f_jac_symbols = []
-        self.g_jac_symbols = []
-
         self.jacobian_store_info = {'dfx': [], 'dfy': [], 'dgx': [], 'dgy': []}
 
     def generate(self):
@@ -95,28 +87,28 @@ class SymProcess:
         """
         Converts string equations into symbolic expressions and lambdifies them.
         """
-        variables = [self.model_storage.stats, self.model_storage.algebs]
+        variables_f_g = [self.model_storage.stats, self.model_storage.algebs]
         equations_f_g = [self.f_list, self.g_list]
         equation_type = ['f', 'g']
-        expr_list = [self.f_list, self.g_list]
 
-        for var_list, equations, eq_type in zip(variables, equations_f_g, equation_type):
-            eq_symb = []
-            var_symb = []
-            for var in var_list:
+        for variables, equations, eq_type in zip(variables_f_g, equations_f_g, equation_type):
+            symbolic_eqs = []
+            symbolic_vars = []
+            #create a list with all symbolic equations (symb_expr) and a list with all symbols in equations (var_symb)
+            for var in variables:
                 if var.eq:
-                    symb_expr = sp.sympify(var.eq)
-                    symb_var = symb_expr.free_symbols
+                    symbolic_expr = sp.sympify(var.eq)
+                    symbols_in_eq = symbolic_expr.free_symbols
 
-                    eq_symb.append(symb_expr)
-                    equations.append(symb_expr)
+                    symbolic_eqs.append(symbolic_expr)
+                    equations.append(symbolic_expr)
 
-                    for symb in symb_var:
-                        if symb not in var_symb:
-                            var_symb.append(symb)
+                    for symb in symbols_in_eq:
+                        if symb not in symbolic_vars:
+                            symbolic_vars.append(symb)
 
             # Lambdify numerical evaluation functions
-            self.lambda_equations[eq_type] = lambdify(var_symb, sp.Matrix(eq_symb), modules='numpy')
+            self.lambda_equations[eq_type] = lambdify(symbolic_vars, sp.Matrix(symbolic_eqs), modules='numpy')
 
         self.f_matrix = sp.Matrix(self.f_list)
         self.g_matrix = sp.Matrix(self.g_list)
@@ -125,31 +117,31 @@ class SymProcess:
         """
         Computes symbolic Jacobian matrices and lambdifies them.
         """
-        self.sym_variables = self.sym_state + self.sym_algeb
-        self.all_variables = self.model_storage.stats + self.model_storage.algebs
+        sym_variables = self.sym_state + self.sym_algeb
+        all_variables = self.model_storage.stats + self.model_storage.algebs
 
         # Compute Jacobian matrices
-        f_jacob_sym = self.f_matrix.jacobian(self.sym_variables) if len(self.f_matrix) > 0 else sp.Matrix([])
-        g_jacob_sym = self.g_matrix.jacobian(self.sym_variables) if len(self.g_matrix) > 0 else sp.Matrix([])
+        f_jacobian_symbolic = self.f_matrix.jacobian(sym_variables) if len(self.f_matrix) > 0 else sp.Matrix([])
+        g_jacobian_symbolic = self.g_matrix.jacobian(sym_variables) if len(self.g_matrix) > 0 else sp.Matrix([])
 
         # Extract unique symbols
-        self.f_jac_symbols = list(f_jacob_sym.free_symbols)
-        self.g_jac_symbols = list(g_jacob_sym.free_symbols)
+        f_jac_symbols = list(f_jacobian_symbolic.free_symbols)
+        g_jac_symbols = list(g_jacobian_symbolic.free_symbols)
 
         # Convert to sparse matrices
-        f_jacob_sym_spa = sp.SparseMatrix(f_jacob_sym)
-        g_jacob_sym_spa = sp.SparseMatrix(g_jacob_sym)
+        f_jacob_symbolic_spa = sp.SparseMatrix(f_jacobian_symbolic)
+        g_jacob_symbolic_spa = sp.SparseMatrix(g_jacobian_symbolic)
 
         # Store Jacobian information
-        for idx, eq_sparse in enumerate([f_jacob_sym_spa, g_jacob_sym_spa]):
+        for idx, eq_sparse in enumerate([f_jacob_symbolic_spa, g_jacob_symbolic_spa]):
             for e_idx, v_idx, e_symbolic in eq_sparse.row_list():
-                var_type = self.all_variables[v_idx].var_type
+                var_type = all_variables[v_idx].var_type
                 eq_var_code = f"d{['f', 'g'][idx]}{var_type}"
                 self.jacobian_store_info[eq_var_code].append((e_idx, v_idx))
 
         # Lambdify Jacobian functions
-        self.jacob_states = lambdify(self.f_jac_symbols, tuple(f_jacob_sym), modules='numpy')
-        self.jacob_algebs = lambdify(self.g_jac_symbols, tuple(g_jacob_sym), modules='numpy')
+        self.jacob_states = lambdify(f_jac_symbols, tuple(f_jacobian_symbolic), modules='numpy')
+        self.jacob_algebs = lambdify(g_jac_symbols, tuple(g_jacobian_symbolic), modules='numpy')
 
     def _rename_func(self, func, func_name, vars=False):
         """
